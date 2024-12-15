2ee5deb59c8437069244915f6af822899ea49f9f
ArtyomKorn1lov <85835332+ArtyomKorn1lov@users.noreply.github.com>
2024-12-15 20:52:32 +0300

    Получение списка аккаунтов с пагинацией и поиском

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
+require("dotenv").config();
 
 const connection = mysql.createConnection({
     host: process.env.dbHost,
@@ -19,10 +20,41 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// Получить список аккаунтов с пагинацией и с поиском
 app.get('/api/accounts/list/', (req, res) => {
-    res.send('Тестовые данные!');
+    let page = req.query.page;
+    let pageCount = req.query.pageCount;
+    let order = req.query.order;
+    let sort = req.query.sort;
+    const searchString = req.query.search;
+    if (!page || !pageCount) {
+        page = 1;
+        pageCount = 20; 
+    }
+    if (!sort || !order) {
+        sort = 'sort';
+        order = 'ASC'
+    }
+    if (sort === 'permission') {
+        sort = 'r.name';
+    } else {
+        sort = 'a.' + sort;
+    }
+    const endLimit = page * pageCount;
+    const startLimit = endLimit - pageCount;
+    let queryString = `SELECT a.id, a.login, a.email, a.picture, r.name FROM accounts as a LEFT JOIN role as r ON a.roleId = r.id`;
+    (typeof searchString !== 'undefined') && (queryString = queryString + ` WHERE a.login LIKE '%${searchString}%' OR a.email LIKE '%${searchString}%'`);
+    queryString = queryString + ` ORDER BY ${sort} ${order} LIMIT ${startLimit}, ${endLimit};`;
+    console.log('final request:', queryString);
+    connection.query(queryString, [], (err, rows, fields) => {
+        if (err) {
+            res.send(err);
+            throw err;
+        }
+        res.send(rows);
+    });
 });
 
 app.listen(port, () => {
-    console.log(`Приложение доступно по порту: ${port}`)
-  });
+    console.log(`Приложение доступно по url: http://localhost:${port}/`)
+});
--- migrations/CreateTables.sql
+++ migrations/CreateTables.sql
@@ -2,19 +2,20 @@ USE fe_task;
 
 CREATE TABLE image (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    path TEXT NOT NULL
+    path TEXT NOT NULL,
 );
 
 CREATE TABLE role (
     id INT PRIMARY KEY AUTO_INCREMENT,
     code VARCHAR(256) NOT NULL,
-    name VARCHAR(256) NOT NULL
+    name VARCHAR(256) NOT NULL,
 );
 
 CREATE TABLE accounts (
 	id INT PRIMARY KEY AUTO_INCREMENT,
     login VARCHAR(256) NOT NULL,
 	email VARCHAR(256) NULL,
+    sort INT DEFAULT 100,
     picture INT NULL,
     FOREIGN	KEY (picture) REFERENCES image (Id),
     roleId INT NOT NULL,
