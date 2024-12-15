3a5541401170c5da2dd8747d37e36dffd18c3d14
ArtyomKorn1lov <85835332+ArtyomKorn1lov@users.noreply.github.com>
2024-12-15 15:07:00 +0300

    Миграции к БД

---
+++ migrations/CreateTables.sql
@@ -0,0 +1,22 @@
+USE fe_task;
+
+CREATE TABLE image (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    path TEXT NOT NULL
+);
+
+CREATE TABLE role (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    code VARCHAR(256) NOT NULL,
+    name VARCHAR(256) NOT NULL
+);
+
+CREATE TABLE accounts (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    login VARCHAR(256) NOT NULL,
+	email VARCHAR(256) NULL,
+    picture INT NULL,
+    FOREIGN	KEY (picture) REFERENCES image (Id),
+    roleId INT NOT NULL,
+    FOREIGN KEY (roleId) REFERENCES role (id)
+);
---
+++ migrations/DropDB.sql
@@ -0,0 +1,1 @@
+DROP DATABASE fe_task;
